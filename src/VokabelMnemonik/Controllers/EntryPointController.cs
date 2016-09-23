3c7d855ec8a1a047b7267111ac664c6b467ce4b6
Gregor Woiwode <gw@co-it.eu>
2016-09-23 14:56:44 +0200

    Sets entry point to root

--- src/VokabelMnemonik/Controllers/EntryPointController.cs
+++ src/VokabelMnemonik/Controllers/EntryPointController.cs
@@ -4,13 +4,29 @@ using VokabelMnemonik.Hypermedia;
 
 namespace VokabelMnemonik.Controllers
 {
-  [Route("[controller]")]
+  /*
+    Entry point = Basisadresse Bsp.: api.vokabel-mnemonik.de/
+    - Dokumentation abrufen
+    - Hypermedia Dokument abrufen
+
+    Oder docs.api.vokabel-mnemonik.de -> Dokumentation
+              api.vokabel-mnemonik.de -> Immer Siren
+
+    Kommt darauf an wie man es modelliert.
+   */
+  [Route("")]
   public class EntryPointController : Controller
   {
     [HttpGet("", Name = "Route_EntryPoint")]
+    // Mit Entity wird Rückgabetyp bereits festgelegt
+    // Es sollte Hypermedia-Typ zurückgegeben werden, damit das Formatieren möglich wird.
     public Entity Get()
     {
+      // Controller ist zuständig, welche Repräsentation versendet wird.
       var document = new DocumentFactory();
+
+      // Mitgeben welcher Mime-Type generiert werden soll.
+      // Media-Type-Formatter: https://damienbod.com/2015/06/03/asp-net-5-mvc-6-custom-protobuf-formatters/
       return document.CreateEntryPoint(Url);
     }
   }
